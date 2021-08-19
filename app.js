275a90364b1449794f0310d973bde3662c95a54b
khaled <shnbyakuya@gmail.com>
2021-08-19 18:45:53 +0100

    fully responsive ~ no validation yet ~~

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-// variables
 
-const button = document.querySelector(".submit");
-const wrapper = document.querySelectorAll(".wrapper");
-
-button.addEventListener("click", (e) => {
-  wrapper.forEach((item) => {
-    const input = document.querySelector(".input");
-    if (!input.checkValidity()) {
-      item.classList.add("invalid");
-    }
-  });
-});
